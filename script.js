d348ae62689b6335f1ac2432802a2aa8ee94bfc0
fhrlkhdz <arukbass@gmail.com>
2021-02-24 14:40:36 +0700

    kedua

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
-        if(this.scroll > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
+        // if(this.scroll > 500){
+        //     $('.scroll-up-btn').addClass("show");
+        // }else{
+        //     $('.scroll-up-btn').removeClass("show");
+        // }
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-    });
+    // $('.scroll-up-btn').click(function(){
+    //     $('html').animate({scrollTop: 0});
+    // });
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
