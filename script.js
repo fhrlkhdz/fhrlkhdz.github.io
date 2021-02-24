08bb6b9a26ddd199588c7e219867aa0358331340
fhrlkhdz <arukbass@gmail.com>
2021-02-24 15:56:07 +0700

    baru

--- script.js
+++ script.js
@@ -1,38 +1,48 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-        if (this.scrollY > 20) {
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
-        // if(this.scroll > 500){
-        //     $('.scroll-up-btn').addClass("show");
-        // }else{
-        //     $('.scroll-up-btn').removeClass("show");
-        // }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
 
     // slide-up script
-    // $('.scroll-up-btn').click(function(){
-    //     $('html').animate({scrollTop: 0});
-    // });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing animation script
-
-    var typed = new Typed(".typing",{
-        strings:["Web Developer","Blogger","Designer","Freelancer"],
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Web Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
-    var typed = new Typed(".typing-2",{
-        strings:["Web Developer","Blogger","Designer","Freelancer"],
+    var typed = new Typed(".typing-2", {
+        strings: ["Web Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
